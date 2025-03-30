68ff8909460c686b408c3b29e85d122bc04470dc
Matic Majerič <majericmatic@gmail.com>
2025-03-30 21:32:00 +0200

    final code fixes

--- client/client.py
+++ client/client.py
@@ -1,21 +1,21 @@
 import requests
-from flask import Flask, render_template_string
+from flask import Flask, send_file, render_template_string
 from threading import Thread
 import time
 import os
 
 app = Flask(__name__)
-IMAGE_PATH = "latest.jpg"
+IMAGE_PATH = "sample.jpeg"
 
 
 def fetch_image():
     while True:
         try:
-            r = requests.get("http://server:5000/image")
+            r = requests.get("http://streznik:5000/image")
             with open(IMAGE_PATH, "wb") as f:
                 f.write(r.content)
         except Exception as e:
-            print("Error:", e)
+            print("Error fetching image:", e)
         time.sleep(10)
 
 
@@ -23,16 +23,20 @@ def fetch_image():
 def index():
     return render_template_string(
         """
-        <html><head><meta http-equiv="refresh" content="10"></head>
-        <body><h1>Client Image Viewer</h1>
-        <img src="/image.jpg" width="640"/></body></html>
-    """
+        <html>
+        <head><meta http-equiv="refresh" content="10"></head>
+        <body>
+            <h1>Client Image Viewer</h1>
+            <img src="/image.jpeg" width="640"/>
+        </body>
+        </html>
+        """
     )
 
 
-@app.route("/image.jpg")
+@app.route("/image.jpeg")
 def serve_image():
-    return open(IMAGE_PATH, "rb").read()
+    return send_file(IMAGE_PATH, mimetype="image/jpeg")
 
 
 if __name__ == "__main__":
--- postavi_sistem.sh
+++ postavi_sistem.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
-
 NETWORK_NAME=slika_net
 CLIENT_NAME=odjemalec
 SERVER_NAME=streznik
+
+docker rm -f $SERVER_NAME 2>/dev/null
+docker rm -f $CLIENT_NAME 2>/dev/null
+docker network rm $NETWORK_NAME 2>/dev/null
 
 docker network create $NETWORK_NAME
 
 docker build ./server -t slika_server
 docker build ./client -t slika_client
 
-docker run -dit --rm --name $SERVER_NAME --network $NETWORK_NAME slika_server
-docker run -dit --rm --name $CLIENT_NAME --network $NETWORK_NAME -p 5001:5001 slika_client
+docker run -dit --name $SERVER_NAME --network $NETWORK_NAME slika_server
+docker run -dit --name $CLIENT_NAME --network $NETWORK_NAME -p 5001:5001 slika_client
 
 CLIENT_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CLIENT_NAME)
 
